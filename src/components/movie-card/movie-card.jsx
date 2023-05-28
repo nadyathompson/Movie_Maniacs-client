65365efd5aaefb63c5e2134d4513253293369363
nadyathompson <127708727+nadyathompson@users.noreply.github.com>
2023-05-27 20:57:48 -0400

    3.4 Connecting API

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({movie, onMovieClick}) => {
     return (
         <div onClick={() => {
@@ -7,3 +9,17 @@ export const MovieCard = ({movie, onMovieClick}) => {
         </div>
     );   
 };
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+      _id: PropTypes.string.isRequired, 
+      Title: PropTypes.string.isRequired,
+      ImagePath: PropTypes.string.isRequired,
+      Director: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+      Year: PropTypes.number.isRequired,
+      Genre: PropTypes.arrayOf(PropTypes.string).isRequired,
+      Featured: PropTypes.bool.isRequired,
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired,
+  };
